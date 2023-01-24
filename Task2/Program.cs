5d86b1f8dac1e53a3469bc3fc8bd128caee2bea1
Ekaterina <ekaterina_r21@mail.ru>
2023-01-24 16:59:35 +0300

    master

---
+++ Task2/Program.cs
@@ -0,0 +1,8 @@
+﻿//Составить алгоритм: если введенное имя совпадает с Вячеслав, то вывести “Привет, Вячеслав”, если нет, то вывести "Нет такого имени"
+
+Console.Write ("Введите имя: ");
+string username = Console.ReadLine();
+if (username == "Вячеслав")
+Console.WriteLine ("Привет, " + username);
+else
+Console.WriteLine ("Нет такого имени");
---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Составить алгоритм: на входе есть числовой массив, необходимо вывести элементы массива кратные 3
+void FillArray(int[]array, int A, int B)
+{
+    for(int i=0; i<array.Length; i++)
+    {
+        array[i] = new Random().Next(A, B);
+    }
+}
+void PrintArray(int[] array)
+{
+    for(int i=0; i< array.Length; i++)
+    {
+        Console.Write(array[i]+ " ");
+    }
+}
+int[]array = new int[5];
+FillArray(array, 1, 100);
+PrintArray(array);
+Console.WriteLine();
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] % 3 == 0)
+    {
+        Console.Write(array[i] );
+    }
+}
