a0ebebc9ba4fc722c2ca267e8637277018892ead
Ekaterina <ekaterina_r21@mail.ru>
2023-01-24 16:28:49 +0300

    Составить алгоритм: если введенное число больше 7, то вывести привет

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Составить алгоритм: если введенное число больше 7, то вывести “Привет”
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if( num > 7)
+{
+    Console.WriteLine("Привет");
+}
+else if ( num == 7)
+{
+    Console.Write("Уже близко");
+}
+else
+{
+    Console.Write("Попытайся снова");
+}
